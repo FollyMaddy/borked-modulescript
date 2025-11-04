164b7deb51c4e16369ba43c0d672cacb52f9ef14
FollyMaddy <meulenf@gmx.net>
2025-11-04 16:44:00 +0100

    V0.2 / miken32 sdl fix / remove depends

--- borked3ds.sh
+++ borked3ds.sh
@@ -11,7 +11,7 @@
 # https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/LICENSE
 #
 
-#Version 0.1
+#Version 0.2
 rp_module_id="borked3ds"
 rp_module_desc="3DS Emulator borked3ds"
 rp_module_help="ROM Extension: .3ds .3dsx .elf .axf .cci .cxi .app\n\nCopy your 3DS roms to $romdir/3ds"
@@ -26,8 +26,10 @@ function depends_borked3ds() {
     fi
     
     #for aarch64 and x86_64 these dependancies are the same
-	local depends=(build-essential cmake clang clang-format libc++-dev libsdl2-dev libssl-dev qt6-l10n-tools qt6-tools-dev qt6-tools-dev-tools  qt6-base-dev qt6-base-private-dev libxcb-cursor-dev libvulkan-dev qt6-multimedia-dev libqt6sql6 libasound2-dev xorg-dev libx11-dev libxext-dev libpipewire-0.3-dev libsndio-dev ffmpeg libgl-dev  libswscale-dev libavformat-dev libavcodec-dev libavdevice-dev libglut3.12 libglut-dev freeglut3-dev mesa-vulkan-drivers) 
-	#use libqt6core6t64 for Trixie or higher
+    #seems to work without, depends that are removed (not tested yet on x86): libc++-dev ffmpeg libavdevice-dev
+    local depends=(build-essential cmake clang clang-format libsdl2-dev libssl-dev qt6-l10n-tools qt6-tools-dev qt6-tools-dev-tools qt6-base-dev qt6-base-private-dev libxcb-cursor-dev libvulkan-dev qt6-multimedia-dev libqt6sql6 libasound2-dev xorg-dev libx11-dev libxext-dev libpipewire-0.3-dev libsndio-dev libgl-dev  libswscale-dev libavformat-dev libavcodec-dev libglut3.12 libglut-dev freeglut3-dev mesa-vulkan-drivers)
+    #use libqt6core6t64 for Trixie or higher
+    
     if compareVersions $__gcc_version lt 14; then
 		depends+=(libqt6core6)
 	else
@@ -84,6 +86,7 @@ function sources_borked3ds() {
  
 function build_borked3ds() {
 	local extra_build_options
+	pushd $md_build/externals/sdl2/SDL && git pull origin release-2.32.x && popd
  	isPlatform "aarch64" && extra_build_options="-DDYNARMIC_USE_BUNDLED_EXTERNALS=OFF"
 	mkdir build
 	cd build
